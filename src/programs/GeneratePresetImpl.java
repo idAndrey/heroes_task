ab012692524c6db9c3995f7f7b341affa236b2d1
idAndrey <55851487+idAndrey@users.noreply.github.com>
2025-01-20 01:09:12 +0300

    Доработан метод getSuitableUnits интерфейса SuitableForAttackUnitsFinder. Удалены отладочные инструкции.

--- src/programs/GeneratePresetImpl.java
+++ src/programs/GeneratePresetImpl.java
@@ -9,292 +9,26 @@ import java.util.*;
 
 public class GeneratePresetImpl implements GeneratePreset {
 
-    public GeneratePresetImpl() {
-    }
-
     @Override
     public Army generate(List<Unit> unitList, int maxPoints) {
 
         // Ваше решение
-        System.out.print("\033\143");
-        System.out.println("generate started\nsortred list");
-
-
 
         Army army = new Army();
         int remainingPoints = maxPoints;
         int maxUnitsDefault = 11;
         List<Unit> newList;
-        //newList = new ArrayList<>();
 
         List<Unit> sortedList;
         sortedList = new ArrayList<>(unitList);
-
-//        List<Unit> sortedList = new ArrayList<>(Comparator.comparingDouble(unit -> {
-//            Unit instance = (Unit) unit;
-//            return (double) instance.getBaseAttack() / instance.getCost() + (double) instance.getHealth() / instance.getCost();
-//        })).reversed();
-//
-//        List<Unit> sortedList = new List<Unit>(Comparator.comparingDouble((Unit::getBaseAttack / (double)Unit::getCost) + Unit::getHealth/Unit::getCost)).reversed();
-
-//        List<Unit> sortedList = new ArrayList<Unit>((Collection) Comparator.comparingDouble(unit -> {
-//            Unit instance = (Unit) unit;
-//            return (double) instance.getBaseAttack() / instance.getCost() + (double) instance.getHealth() / instance.getCost();
-//        })).reversed();
-
-//            List<Unit> sortedList = new ArrayList<>(Comparator.comparingDouble(unit -> {
-//                Unit instance = (Unit) unit;
-//                return (double) instance.getBaseAttack() / instance.getCost() + (double) instance.getHealth() / instance.getCost();
-//            })).reversed();
-
-//        List<Unit> sortedList = new ArrayList<Unit>((Collection) Comparator.comparingDouble(unit -> (
-//                (double) unit.getBaseAttack() / unit.getCost() + (double) unit.getHealth() / unit.getCost()
-//                )).reversed());
-
-
-        System.out.println("Количество юнитов: " + unitList.size());
-        System.out.println("Количество очков: " + maxPoints);
-
-        System.out.println();
-        System.out.println(unitList.toString());
-        unitList.stream().forEach(unit -> System.out.println(unit.getUnitType() + " " + unit.getCost()+ " x = " + unit.getxCoordinate() + " y = " + unit.getyCoordinate() + "\n"));
-        System.out.println();
-
-     /*   System.out.println("Сведения о списке юнитов - LIST\n");
-        for (Unit unit : unitList) {
-            System.out.println("Сведения о юните - LIST\n");
-            String out = "";
-            //out = unit.toString();
-            out = out + "Name: " + unit.getName() + "\n";
-            out = out + "Type: " + unit.getUnitType() + "\n";
-            out = out + "Health: " + unit.getHealth() + "\n";
-            out = out + "BaseAttack: " + unit.getBaseAttack() + "\n";
-            out = out + "Cost: " + unit.getCost() + "\n";
-            out = out + "AttackType: " + unit.getAttackType() + "\n";
-            out = out + "AttackBonuses: " + unit.getAttackBonuses() + "\n";
-            out = out + "DefenceBonuses: " + unit.getDefenceBonuses() + "\n";
-            out = out + "X coordinate: " + unit.getxCoordinate() + "\n";
-            out = out + "Y coordinate: " + unit.getyCoordinate() + "\n";
-
-            //System.out.println(out + "\n");
-        }
-        System.out.println("Сведения о списке юнитов - LIST\n\n");
-*/
-
-
-        /*System.out.println("Сведения о списке юнитов - SORTED\n");
-        for (Unit unit : sortedList) {
-            System.out.println("Сведения о юните - SORTED\n");
-            String out = "";
-            //out = unit.toString();
-            out = out + "Name: " + unit.getName() + "\n";
-            out = out + "Type: " + unit.getUnitType() + "\n";
-            out = out + "Health: " + unit.getHealth() + "\n";
-            out = out + "BaseAttack: " + unit.getBaseAttack() + "\n";
-            out = out + "Cost: " + unit.getCost() + "\n";
-            out = out + "AttackType: " + unit.getAttackType() + "\n";
-            out = out + "AttackBonuses: " + unit.getAttackBonuses() + "\n";
-            out = out + "DefenceBonuses: " + unit.getDefenceBonuses() + "\n";
-            out = out + "X coordinate: " + unit.getxCoordinate() + "\n";
-            out = out + "Y coordinate: " + unit.getyCoordinate() + "\n";
-
-            //System.out.println(out + "\n");
-        }
-        System.out.println("Сведения о списке юнитов - SORTED\n\n");
-*/
-        /*System.out.println("Сведения о юнитах");
-        for (Unit unit : unitList) {
-            //System.out.println("Сведения о юните - ЮНИТ\n");
-            String out = "";
-            out = "" + unit.getName() + unit.getUnitType();
-           // System.out.println(out);
-        }*/
-        //System.out.println("Сведения о юнитах-  КОНЕЦ");
-
-        /*sortedList.stream().peek(unit -> {
-            unit.setxCoordinate();
-            newList.add(unit)
-        });*/
-
-    //    newList = new ArrayList<>(sortedList);
-     //   LinkedList unitLinkedList = new LinkedList(sortedList);
-
-
-
-
-     /*   HashMap hashMap = new HashMap();
-        Random randomInt = new Random();
-
-        while(maxPoints > 0 && !sortedList.isEmpty()) {
-            Unit nextUnit;
-            String nextUnitType = (nextUnit = (Unit) unitLinkedList.peek()).getUnitType();
-            int unitCost = nextUnit.getCost();
-            int amountUnits;
-            if ((amountUnits = (Integer) hashMap.getOrDefault(nextUnitType, 0)) < 11 && maxPoints >= unitCost) {
-                int[] coordXY;
-                if ((coordXY = this.findAvailableCoordinates(var4, nextUnitType, randomInt, 0)) != null) {
-                    int coordX = coordXY[0];
-                    int coordY = coordXY[1];
-                    ++amountUnits;
-                    hashMap.put(nextUnitType, amountUnits);
-                    nextUnitType = nextUnitType + " " + amountUnits;
-                    nextUnit = new Unit(nextUnitType, nextUnit.getUnitType(), nextUnit.getHealth(), nextUnit.getBaseAttack(), nextUnit.getCost(), nextUnit.getAttackType(), nextUnit.getAttackBonuses(), nextUnit.getDefenceBonuses(), coordX, coordY);
-
-                    while(true) {
-                        var4.add(nextUnit);
-                        System.out.println("Added " + var4.size() + " unit");
-                        army.getUnits().add(nextUnit);
-                        maxPoints -= unitCost;
-                        var7 += unitCost;
-                        if ((a + 1) % 2 == 0) {
-                            continue;
-                        }
-                    }
-                } else {
-                    System.out.println("Not found units coordinates: " + nextUnitType);
-                }
-            }
-*/
-   /*     int i = 0;
-        for(Unit unit : newList) {
-
-            unit.setName(unit.getName() + " " + i);
-            i++;
-        }
-        newList.forEach(unit -> {
-            Random random = new Random();
-            int x = random.nextInt(3);
-            int y = random.nextInt(21);
-
-            unit.setxCoordinate(x);
-            unit.setyCoordinate(y);
-
-            //System.out.println("FOR Координаты " + unit.getName() + " x = " + unit.getxCoordinate() + " y = " + unit.getyCoordinate() + "\n");
-        });
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("Координаты:\n");
-
-        newList.stream().forEach(unit -> System.out.println(unit.getName() + " " + unit.getCost()+ " x = " + unit.getxCoordinate() + " y = " + unit.getyCoordinate() + "\n"));
-*/
-
-//        sortedList.sort((unit1, unit2) -> {
-//            double efficiency1 = (double)unit1.getBaseAttack() / (double)unit1.getCost();
-//            double efficiency2 = (double)unit2.getBaseAttack() / (double)unit2.getCost();
-//            if (Double.compare(efficiency2, efficiency1) != 0) {
-//                System.out.println("BaseAttack/Cost сортирую " + unit1.toString() + " и " + unit2.toString());
-//
-//                return Double.compare(efficiency2, efficiency1);
-//            } else {
-//                double efficiency3 = (double)unit1.getHealth() / (double)unit1.getCost();
-//                double efficiency4 = (double)unit2.getHealth() / (double)unit2.getCost();
-//                System.out.println("Health/Cost сортирую " + unit1.toString() + " и " + unit2.toString());
-//
-//                return Double.compare(efficiency4, efficiency3);
-//            }
-//        });
-
-//        sortedList.sort(Comparator.comparingDouble(unit -> {
-//            double efficiency1 = (double)unit.getBaseAttack() / (double)unit.getCost();
-//            double efficiency2 = (double)unit.getHealth() / (double)unit.getCost();
-//            return efficiency1 + efficiency2;
-//        }).reversed());
-
-//        SUITABLE CASE
-//        sortedList.sort(Comparator.comparingDouble( u -> ((double) u.getBaseAttack()) ));
-
-        System.out.println("\n\nUNSORTED:\n");
-        sortedList.stream().forEach(unit -> System.out.println(
-                "Unit: " + unit.getUnitType() + "\t" +
-                "Cost\t" + unit.getCost()+"\t" +
-                "Attack\t" + unit.getBaseAttack() + "\t" +
-                "\tHealth\t" + unit.getHealth() + "\n" +
-                "\tBaseAttack at Cost\t" +
-                (double) unit.getBaseAttack() / (double)unit.getCost() + "\n" +
-                "\tHealth at Cost\t\t" +
-                (double)unit.getHealth() / (double)unit.getCost() + "\n" +
-                "\tEfficiency\t\t\t\t" +
-                ((double) unit.getBaseAttack() / (double)unit.getCost() + (double)unit.getHealth() / (double)unit.getCost()) +
-                "\n"
-        ));
-
 
         sortedList.sort(Comparator.comparingDouble( u -> {
             double efficiency1 = (double)u.getBaseAttack() / (double)u.getCost();
             double efficiency2 = (double)u.getHealth() / (double)u.getCost();
             return efficiency1 + efficiency2;
-//            return (double) u.getBaseAttack();
         }) );
 
-//        System.out.println("SORTED unitlist:");
-//        sortedList.stream().forEach(unit -> System.out.println(
-//                unit.getUnitType() + " " +
-//                        unit.getCost()+"\n" +
-//                        (double) unit.getBaseAttack() / (double)unit.getCost() + " " +
-//                        (double)unit.getHealth() / (double)unit.getCost() +
-//                        "\n\n"));
-        System.out.println("\n\nSORTED unitlist:\n");
-        sortedList.stream().forEach(unit -> System.out.println(
-                "Unit: " + unit.getUnitType() + "\t" +
-                "Cost\t" + unit.getCost()+"\t" +
-                "Attack\t" + unit.getBaseAttack() + "\t" +
-                "\tHealth\t" + unit.getHealth() + "\n" +
-                "\tBaseAttack at Cost\t" +
-                (double) unit.getBaseAttack() / (double)unit.getCost() + "\n" +
-                "\tHealth at Cost\t\t" +
-                (double)unit.getHealth() / (double)unit.getCost() + "\n" +
-                "\tEfficiency\t\t\t\t" +
-                ((double) unit.getBaseAttack() / (double)unit.getCost() + (double)unit.getHealth() / (double)unit.getCost()) +
-                "\n"
-        ));
-
         Collections.reverse(sortedList);
-
-//        System.out.println("reversed:");
-//        sortedList.stream().forEach(unit -> System.out.println(
-//                unit.getUnitType() + " " +
-//                        unit.getCost()+"\n" +
-//                        (double) unit.getBaseAttack() / (double)unit.getCost() + " " +
-//                        (double)unit.getHealth() / (double)unit.getCost() +
-//                        "\n\n"));
-        System.out.println("\n\nreversed:\n");
-        sortedList.stream().forEach(unit -> System.out.println(
-                "Unit: " + unit.getUnitType() + "\t" +
-                "Cost\t" + unit.getCost()+"\t" +
-                "Attack\t" + unit.getBaseAttack() + "\t" +
-                "\tHealth\t" + unit.getHealth() + "\n" +
-                "\tBaseAttack at Cost\t" +
-                (double) unit.getBaseAttack() / (double)unit.getCost() + "\n" +
-                "\tHealth at Cost\t\t" +
-                (double)unit.getHealth() / (double)unit.getCost() + "\n" +
-                "\tEfficiency\t\t\t\t" +
-                ((double) unit.getBaseAttack() / (double)unit.getCost() + (double)unit.getHealth() / (double)unit.getCost()) +
-                "\n"
-        ));
-        stopPrint();
-//
-//            double efficiency1 = (double)unit.getBaseAttack() / (double)unit.getCost();
-//            double efficiency2 = (double)unit.getHealth() / (double)unit.getCost();
-//            return efficiency1 + efficiency2;
-//        }).reversed());
-
-
-        // Sort units by efficiency: attack/cost and health/cost
-        // sortedList.sort(Comparator.comparingDouble(unit -> -((double) unit.getBaseAttack() / unit.getCost() + (double) unit.getHealth() / unit.getCost())));
-
-
-
-        sortedList.stream().forEach(unit -> System.out.println(unit.getUnitType() + " " + unit.getCost()+"\n"+ (double)unit.getBaseAttack() / (double)unit.getCost() + " " + (double)unit.getHealth() / (double)unit.getCost() + "\n\n"));
-
-
-
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
 
         newList = new ArrayList<>();
         for (Unit unit : sortedList) {
@@ -314,8 +48,8 @@ public class GeneratePresetImpl implements GeneratePreset {
                         unit.getAttackBonuses(),
                         unit.getDefenceBonuses(),
                         0,
-                        0));
-
+                        0)
+                );
             }
         }
 
@@ -335,30 +69,10 @@ public class GeneratePresetImpl implements GeneratePreset {
                     //return new int[]{coordX, coordY};
                 }
                 ++tempCounter;
-
-
-
             }
         }
-
 
         army.setUnits(newList);
-
-//        army.setUnits(newList);
         return army;
-    }
-
-    public void stopPrint(){
-        int inChar;
-        String s = "";
-        try {
-            inChar = System.in.read();
-            while (System.in.available() > 0) {
-                s += (char) inChar;
-                inChar = System.in.read();
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка");
-        }
     }
 }
--- src/programs/SimulateBattleImpl.java
+++ src/programs/SimulateBattleImpl.java
@@ -16,20 +16,6 @@ public class SimulateBattleImpl implements SimulateBattle {
 
         // Ваше решение
 
-        System.out.println("simulate started");
-
-//        int inChar;
-//        String s = "";
-//        try {
-//            inChar = System.in.read();
-//            while (System.in.available() > 0) {
-//                s += (char) inChar;
-//                inChar = System.in.read();
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Ошибка");
-//        }
-
         List<Unit> playerUnits;
         playerUnits = new ArrayList<>();
         playerUnits.addAll(playerArmy.getUnits());
@@ -59,11 +45,11 @@ public class SimulateBattleImpl implements SimulateBattle {
                 playerUnitsAlive = playerArmy.getUnits().stream().filter(Unit::isAlive).toList();
                 computerUnitsAlive = computerArmy.getUnits().stream().filter(Unit::isAlive).toList();
 
-                playerUnitsQueue.addAll(playerUnitsAlive.stream().filter((var1x) -> {
-                    return !hashSet.contains(var1x);
+                playerUnitsQueue.addAll(playerUnitsAlive.stream().filter((unit1) -> {
+                    return !hashSet.contains(unit1);
                 }).toList());
-                computerUnitsQueue.addAll(computerUnitsAlive.stream().filter((var1x) -> {
-                    return !hashSet.contains(var1x);
+                computerUnitsQueue.addAll(computerUnitsAlive.stream().filter((unit2) -> {
+                    return !hashSet.contains(unit2);
                 }).toList());
 
                 label64:
@@ -95,97 +81,19 @@ public class SimulateBattleImpl implements SimulateBattle {
 
                             System.out.println("stopFlag " + stopFlag);
                             break label64;
-//        int inChar;
-//        String s = "";
-//        try {
-//            inChar = System.in.read();
-//            while (System.in.available() > 0) {
-//                s += (char) inChar;
-//                inChar = System.in.read();
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Ошибка");
-//        }
-
-
-
-
-
                         }
                     }
 
                     hashSet.add(computerUnit);
-                        System.out.println("continue ");
                 }
 
                 if (stopFlag) {
                     ++rounds;
                     hashSet.clear();
-                    System.out.println();
-                    System.out.println("Round " + rounds + " is over!");
-                    System.out.println("Player army has " + playerUnitsAlive.size() + " units");
-                    System.out.println("Computer army has " + computerUnitsAlive.size() + " units");
-                    System.out.println();
                 }
-
-//                if (playerUnitsAlive.size() != 0 && computerUnitsAlive.size() != 0) {
-//                  if (playerUnitsAlive.size() != 0 && computerUnitsAlive.size() != 0) {
-//                       continue;
-//                   }
-//
-//                    return;
-//                }
-
-                System.out.println("Battle is over!");
                 return;
             }
         }
-
-//        for( Unit unit : playerUnits) {
-//            if (unit.isAlive()) {
-//                playerHasAlive = true;
-//                break;
-//            }
-//        }
-//
-//        for( Unit unit : computerUnits) {
-//            if (unit.isAlive()) {
-//                computerHasAlive = true;
-//                break;
-//            }
-//        }
-//
-//        boolean playerHasAlive = false;
-//        boolean computerHasAlive = false;
-//
-//        while (playerHasAlive && computerHasAlive) {
-//            allUnits.sort((u1, u2) -> Integer.compare(u2.getBaseAttack(), u1.getBaseAttack()));
-//
-//            for (Unit unit : allUnits) {
-//                if (!unit.isAlive()) continue;
-//                Unit target = unitAttack(unit);
-//
-//
-//            }
-//
-//            for( Unit unit : playerUnits) {
-//                if (unit.isAlive()) {
-//                    playerHasAlive = true;
-//                    break;
-//                }
-//            }
-//
-//            for( Unit unit : computerUnits) {
-//                if (unit.isAlive()) {
-//                    computerHasAlive = true;
-//                    break;
-//                }
-//            }
-//
-//        }
-
-//        System.out.println("simulate finished");
-
     }
     private Unit unitAttack(Unit attackUnit) throws InterruptedException {
         Unit targetUnit = attackUnit.getProgram().attack();
